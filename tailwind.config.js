5e30b879893fe6234ced0d0a2c6644d4acdfebb2
programming hero <meherun2002@gmail.com>
2025-11-08 22:37:35 +0600

    last commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+    // tailwind.config.js (for v3.x and below)
+    module.exports = {
+      content: [
+        './pages/**/*.{html,js}',
+        './components/**/*.{html,js}',
+      ],
+      // ... other configurations
+    }
